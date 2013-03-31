7aa86dbfb42c044a5b4d367be56318d083d68aae
ace6598 <ace6598@gmail.com>
2013-03-31 01:47:37 -0400

    fixed broken shit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 from flask import render_template
 from flask import request
-import sendgrid
+#import sendgrid
 
-s = sendgrid.Sendgrid('ace6598','aiser12',secure=True)
+#s = sendgrid.Sendgrid('ace6598','aiser12',secure=True)
 app = Flask(__name__)
 
 @app.route("/")
